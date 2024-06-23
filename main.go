b379547d22a8db1e3e902165084173d8b9f6cc8c
Sakkarin Saysai <oat.saysai@gmail.com>
2024-06-23 14:46:02 +0700

    Write data to CSV

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/csv"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -37,17 +39,7 @@ func main() {
 	if *runLambda {
 		lambda.Start(handler)
 	} else {
-
-		sampleData, err := mariaDB.ListSampleData(db)
-		if err != nil {
-			log.Fatal(err)
-		}
-		// fmt.Println(sampleData)
-
-		for _, row := range sampleData {
-			fmt.Println(row)
-		}
-
+		genReport001()
 	}
 }
 
@@ -59,7 +51,47 @@ func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 	for _, msg := range sqsEvent.Records {
 		fmt.Printf("Got SQS message %q with body %q\n", msg.MessageId, msg.Body)
 		// TODO: Add application logic here
+		genReport001()
 	}
 
 	return nil
+}
+
+func genReport001() {
+	sampleData, err := mariaDB.ListSampleData(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Gen CSV
+	file, err := os.Create("report_001.csv")
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+	defer file.Close()
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	// Write CSV header
+	w.Write([]string{
+		"name",
+		"data_001",
+		"data_002",
+		"created_time",
+		"updated_time",
+	})
+
+	for _, data := range sampleData {
+		row := []string{
+			data.Name,
+			data.Data_001,
+			data.Data_002,
+			data.CreatedTime.Local().String(),
+			data.UpdatedTime.Local().String(),
+		}
+		if err := w.Write(row); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
 }
